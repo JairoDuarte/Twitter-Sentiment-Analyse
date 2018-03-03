9577600b347357c1a179691ae2e37dbb386b8ec4
Jairo Duarte <alfredojairo17@hotmail.com>
2018-03-03 02:24:17 +0000

    first commit , add twitter configuration api

---
+++ twitter_config.py
@@ -0,0 +1,7 @@
+"""Ces informations proviennent de l'API Twitter
+après avoir crée un compte twitter je les ai obtenu du site apps.twitter.com"""
+
+CONSUMER_KEY = 'VaudfvTL97lNMsuLdmULmrjOx'
+CONSUMER_SECRET = 'dVRTdAHsDfgglwfnGnTxr7UQ1lClZptMbBUUe4pmOLnVK7sjA4'
+ACCESS_TOKEN = '757660045798670336-B9RTrk9i2H0njV18oaVUrVu5U3FBQVL'
+ACCESS_TOKEN_SECRET = 'a1REY2SDd7OMd3DN55Y4PrQg5dsX6nxSs68OwXvj8YPqW' # pylint: disable=invalid-name
